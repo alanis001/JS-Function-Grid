2602ceffd5e0c904f677fe1d0d42d5828ddd6138
Daril Alanis <a.alanis1108@gmail.com>
2017-01-17 21:48:50 +0000

    smailey face

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Write your function calls below this line
+/*
+Smileyface
+setColor(3,3, "black");
+setColor(4,3, "black");
+setColor(3,6, "black");
+setColor(4,6, "black");
+setColor(6,2, "black");
+setColor(6,7, "black");
+setColor(7,6, "black");
+setColor(7,3, "black");
+setColor(7,4, "black");
+setColor(7,5, "black");
+/*
+
